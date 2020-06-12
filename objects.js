b495da4972e90d9c2f237366e99d597353dd6c76
Cedric Patton <ceddyp117@gmail.com>
2020-06-12 14:31:13 +0000

    Automatically backed up by Learn

--- objects.js
+++ objects.js
@@ -1,5 +1,5 @@
 var playlist = new Object({keys: '0'});
 
-function updatePlayList(playList, artistName, songTitle){
+function updatePlayList(playList, Phil Ochs, songTitle){
   Object.assign({}, playList,{ [Phil Ochs] : "Here's to the State of Mississippi" )
 }
