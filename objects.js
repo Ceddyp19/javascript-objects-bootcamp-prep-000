b0fb6e37d26bbb27d2003f6648d2c45384ee06ec
Cedric Patton <ceddyp117@gmail.com>
2020-06-12 14:45:58 +0000

    Automatically backed up by Learn

--- objects.js
+++ objects.js
@@ -1,9 +1,9 @@
 var playlist = new Object({Lizzo: 'Truth Hurts'});
 
-/*function updatePlaylist(playlist, artistName, songTitle) {
+function updatePlaylist(playlist, artistName, songTitle) {
   playlist[artistName] = songTitle
   return playlist
-}*/
+}
 
 /*function removeFromPlaylist(playlist, artistName) {
   delete playlist[artistName]
